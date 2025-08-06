2e24795f0f7cfaeb9f8beb6c37f24731a4084dfe
DIVYASHREE M T <divyashree.vts@gmail.com>
2025-08-06 22:13:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function sendMessage() {
   try {
     // Set up a timeout controller for 20 seconds
     const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 20000); // 20s
+    const timeoutId = setTimeout(() => controller.abort(), 40000); // 40s
 
     const response = await fetch(RELAY_WEBHOOK_URL, {
       method: "POST",
